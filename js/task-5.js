234f6048a3d1bf437de16365a6178fe0e2c55e81
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-05-06 20:49:11 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,12 @@
+const changeColorBtn = document.querySelector('.change-color');
+changeColorBtn.addEventListener('click', (event) => {
+  const body = document.querySelector('body');
+  const colorName = document.querySelector('.color');
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+  changeColorBtn.blur();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
