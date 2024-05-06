08560e954c04253b7c6e380309a13a5a1acae484
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-05-06 20:08:36 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', pressSubmit);
+
+function pressSubmit(event) {
+    event.preventDefault();
+    const user = {};
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    if (email.trim() === '' || password.trim() === '') {
+        alert('All form fields must be filled in');
+    } else {
+        user.email = email.trim();
+        user.password = password.trim();
+        console.log(user);
+        form.reset();
+    }
+}
